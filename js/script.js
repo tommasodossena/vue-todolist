ca3ed552f363d6723264ac461656ca751905d130
Tommaso <dossena.tommaso.dt@gmail.com>
2021-03-26 15:57:40 +0100

    To-do add/remove script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 var vm = new Vue(
     {
         el: "#root",
+        data: {
+            tasks: 
+            [
+                "Svolgere esercizio to-do list utilizzando Vue.js",
+                "Riscrivere i concetti base appresi a lezione",
+                "Studiare documentazione Vue.js"
+            ],
+            newTask: "",
+        },
+        methods: {
+            addTask: function() {
+                if ( this.newTask != "" ) {
+                    this.tasks.push(this.newTask);
+                    this.newTask = "";   
+                }
+            },
+            deleteTask: function(taskIndex) {
+                this.tasks.splice(taskIndex, 1);
+            }
+        }
     }
 );
